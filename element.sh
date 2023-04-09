60741e3dbd5d1b3772d116435ce44340a8f06347
Mark Dexter <a.markdexter@gmail.com>
2023-04-09 16:52:26 +0800

    Update element.sh

--- element.sh
+++ element.sh
@@ -8,13 +8,10 @@ then
   echo "Please provide an element as an argument."
 
 else 
-  # when there is an argument
-
-  # If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output only The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
 
   if [[ $1 =~ ^[0-9]*$ ]]
   then
-    # if a number save to atomic_number variable
+    
     ATOMIC_NUMBER_ARG=$1
 
     ATOMIC_RESULT=$($PSQL "SELECT atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id, symbol, name FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER_ARG;")
@@ -35,7 +32,6 @@ else
   elif [[ $1 =~ (^[A-Z][a-z]{0,2}$) ]]
    then
 
-    # echo "A symbol"
 
     SYMBOL_ARG=$1
 
@@ -56,7 +52,24 @@ else
 
   else
 
-    echo "not a valid argument"
+    NAME_ARG=$1
+
+    NAME_RESULT=$($PSQL "SELECT atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id, symbol, name FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE name='$NAME_ARG';")
+
+    if [[ -z $NAME_RESULT ]]
+    then
+
+      echo "I could not find that element in the database."
+
+    else 
+
+      echo "$NAME_RESULT" | while IFS='|' read -r ATOMIC_NUMBER TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID SYMBOL NAME
+      do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It'sa $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      done
+
+    fi
+
 
   fi
     
