cf10aa9f48fa8f8a90b87c1df929101060e0c858
Mark Dexter <a.markdexter@gmail.com>
2023-04-09 16:58:04 +0800

    clear some logs and finishing the challenge

--- element.sh
+++ element.sh
@@ -8,10 +8,13 @@ then
   echo "Please provide an element as an argument."
 
 else 
+  # when there is an argument
+
+  # If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output only The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
 
   if [[ $1 =~ ^[0-9]*$ ]]
   then
-    
+ 
     ATOMIC_NUMBER_ARG=$1
 
     ATOMIC_RESULT=$($PSQL "SELECT atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id, symbol, name FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER_ARG;")
@@ -32,7 +35,6 @@ else
   elif [[ $1 =~ (^[A-Z][a-z]{0,2}$) ]]
    then
 
-
     SYMBOL_ARG=$1
 
     SYMBOL_RESULT=$($PSQL "SELECT atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id, symbol, name FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$SYMBOL_ARG';")
@@ -46,7 +48,7 @@ else
 
       echo "$SYMBOL_RESULT" | while IFS='|' read -r ATOMIC_NUMBER TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID SYMBOL NAME
       do
-        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It'sa $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
       done
     fi
 
@@ -65,7 +67,7 @@ else
 
       echo "$NAME_RESULT" | while IFS='|' read -r ATOMIC_NUMBER TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID SYMBOL NAME
       do
-        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It'sa $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
       done
 
     fi
